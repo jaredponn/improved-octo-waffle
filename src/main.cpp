8141b1653b0a6f2eecebd0c4c221c55d3199c954
chinahehaiyang <imhhy123@hotmail.com>
2018-11-13 16:54:10 -0700

    change to graphics

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include "packedvector.h"
-
+#include "graphics.h"
+#include <SFML/Graphics.hpp>
 int main(void)
 {
+	sf::RenderWindow window(sf::VideoMode(500, 500), "waffle",
+				sf::Style::Close);
 	iow::PackedVector<int> a(121);
 	a.add_element_at_sparse_vector(1, 23);
 	a.delete_element_at_sparse_vector(1);
+	// my bad graphics example:
+	sf::RectangleShape rect1;
+	// create the Vector2 vectors for the size, position, and color of the
+	// shape
+	sf::Vector2f sizeOfRect(40, 60);
+	sf::Vector2f posOfRect(10, 20);
+	sf::Color colorOfRect(100, 200, 250);
+	// call the mutateRectangle in graphics.h to change the rectangle's size
+	// and stuff.. can reuse to update its position
+	iow::mutateRectangle(rect1, sizeOfRect, posOfRect, colorOfRect);
 
+	// same thing here for the circle ;)
+	sf::CircleShape cir1;
+	float sizeOfCir = 80;
+	sf::Vector2f posOfCir(400, 50);
+	sf::Color colorOfCir(100, 200, 250);
+	iow::mutateCircle(cir1, sizeOfCir, posOfCir, colorOfCir);
+
+	// displaying them
+	while (1) {
+		window.draw(rect1);
+		window.display();
+		window.draw(cir1);
+		window.display();
+	}
 
 	std::cout << "hellow";
 	return 0;
