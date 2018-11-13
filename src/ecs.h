681f0fb7020fb99b26038e3b298f1a3b6e680105
jaredponn <jpon1234@gmail.com>
2018-11-12 23:55:56 -0700

    added stack to the ecs. still needs to have the appropriate create and delete entity functions

--- src/ecs.h
+++ src/ecs.h
@@ -1,6 +1,32 @@
 #pragma once
+#include <stack>
 #include "packedvector.h"
 
+namespace iow
+{
 class ECS
 {
+    private:
+	std::stack<size_t> m_free_indices;
+
+    public:
+	ECS(size_t capacity);
+	// TODO
+	// put more component packed vectors here
+
+
+	// TODO add delete and add entity functions here
 };
+
+ECS::ECS(size_t capacity)
+{
+	for (size_t i = 0; i < capacity; ++i) {
+		m_free_indices.push(i);
+	}
+	// TODO
+	// make sure you intialize the compoenets as well. probably use xmacros
+	// to do this
+}
+
+
+}; // namespace iow
