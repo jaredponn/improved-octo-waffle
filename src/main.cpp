bb9b14166f3d1f393e4706217faecaf8965b264a
jaredponn <jpon1234@gmail.com>
2018-11-14 10:13:15 -0700

    cleaned up the garbage

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,34 @@
-#include <iostream>
-#include <vector>
-#include "packedvector.h"
-#include "graphics.h"
 #include <SFML/Graphics.hpp>
+#include <vector>
+
 #include "input.h"
+#include "ecs.h"
+#include "graphics.h"
+#include "packedvector.h"
+
+constexpr unsigned int MAX_ENTITIES = 1000000;
+
 int main(void)
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "waffle",
 				sf::Style::Close);
-
 	iow::InputManager::initInputManager();
-	// my bad graphics example:
-	sf::RectangleShape rect1;
-	// create the Vector2 vectors for the size, position, and color of the
-	// shape
-	sf::Vector2f sizeOfRect(40, 60);
-	sf::Vector2f posOfRect(10, 20);
-	sf::Color colorOfRect(100, 200, 250);
-	// call the mutateRectangle in graphics.h to change the rectangle's size
-	// and stuff.. can reuse to update its position
-	iow::mutateRectangle(rect1, sizeOfRect, posOfRect, colorOfRect);
+	iow::ECS ecs(MAX_ENTITIES);
 
-	// same thing here for the circle ;)
-	sf::CircleShape cir1;
-	float sizeOfCir = 80;
-	sf::Vector2f posOfCir(400, 50);
-	sf::Color colorOfCir(100, 200, 250);
-	iow::mutateCircle(cir1, sizeOfCir, posOfCir, colorOfCir);
-	// main game loop
-	// displaying them
-	while (1) {
+	sf::Clock clock;
+	sf::Time ti = sf::microseconds(0);
+	sf::Time tf = sf::microseconds(0);
+	sf::Time dt = sf::microseconds(0);
+
+	while (window.isOpen()) {
+		ti = clock.getElapsedTime();
 		iow::InputManager::shiftAndUpdateInputbuffer();
-		window.draw(rect1);
-		window.display();
-		window.draw(cir1);
-		window.display();
+
+		ecs.runECS(dt, iow::InputManager::getKeyBuffer());
+
+
+		tf = clock.getElapsedTime();
+		dt = tf = ti;
 	}
 
 	return 0;
