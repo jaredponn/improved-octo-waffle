df0edb71a7b4061429925da2c7af9e55425b6af3
jaredponn <jpon1234@gmail.com>
2018-11-14 09:35:05 -0700

    fixed non compiling code...

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <vector>
 #include "packedvector.h"
-<<<<<<< HEAD
 #include "graphics.h"
 #include <SFML/Graphics.hpp>
 #include "input.h"
-	int main(void)
+int main(void)
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "waffle",
 				sf::Style::Close);
 
 	iow::InputManager::initInputManager();
-	iow::PackedVector<int> a(121);
-	a.add_element_at_sparse_vector(1, 23);
-	a.delete_element_at_sparse_vector(1);
 	// my bad graphics example:
 	sf::RectangleShape rect1;
 	// create the Vector2 vectors for the size, position, and color of the
@@ -34,7 +30,7 @@
 	// main game loop
 	// displaying them
 	while (1) {
-		iow::inputmanager::shiftandupdateinputbuffer();
+		iow::InputManager::shiftAndUpdateInputbuffer();
 		window.draw(rect1);
 		window.display();
 		window.draw(cir1);
