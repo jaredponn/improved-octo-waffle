8db20412439a1348bb3242a8db193651ef19c10b
jaredponn <jpon1234@gmail.com>
2018-11-14 10:12:57 -0700

    chagned docs, writinga render function

--- src/graphics.h
+++ src/graphics.h
@@ -4,9 +4,18 @@
 namespace iow
 {
 
+void pushToRenderBuffer();
+
+
+// takes in the size, xcord, ycord, and color of the
+// rectangle
 void mutateRectangle(sf::RectangleShape &rect, const sf::Vector2f size,
 		     const sf::Vector2f pos, const sf::Color color);
+
+// takes in the size, xcord, ycord, and color of the
+// rectangle
 void mutateCircle(sf::CircleShape &circle, const float radius,
 		  const sf::Vector2f pos, const sf::Color color);
+
 
 } // namespace iow
