011961cd4337b6ff8ab7aa35d2d9bf5b1e4a2478
jaredponn <jpon1234@gmail.com>
2018-11-17 10:31:01 -0700

    whitespacing

--- src/collision.h
+++ src/collision.h
@@ -5,6 +5,12 @@
 namespace iow
 {
 
+/*
+ * get the updated position
+ * checks if there is a collision
+ *time is micro seconds
+ */
+
 // checks collision between 2 rectangles
 std::optional<sf::Vector2f> checkCollision(sf::RectangleShape entity1,
 					   sf::RectangleShape entity2);
@@ -12,8 +18,3 @@ std::optional<sf::Vector2f> checkCollision(sf::RectangleShape entity1,
 // checks collision between a circle and a rectangle
 bool checkCollision(sf::CircleShape entity1, sf::RectangleShape entity2);
 } // namespace iow
-  /*
-   * get the updated position
-   * checks if there is a collision
-   *time is micro seconds
-   */
