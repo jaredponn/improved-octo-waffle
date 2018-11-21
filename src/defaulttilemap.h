5d86af39f728c8b9db3344501bd652454520e456
jaredponn <jpon1234@gmail.com>
2018-11-21 14:34:27 -0700

    file for testing of a tile map

---
+++ src/defaulttilemap.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace iow
+{
+
+// clang-format off
+constexpr static char DEFAULT_TILE_MAP[] = 
+"1 1 1 1 1\n"
+"1 0 0 0 1\n"
+"1 0 0 0 1\n"
+"1 0 0 0 1\n"
+"1 1 1 1 1\n"
+;
+// clang-format on
+
+} // namespace iow
