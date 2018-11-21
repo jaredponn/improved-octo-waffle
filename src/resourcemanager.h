2c219b673e4c31e8778d3534c6783dc8ff5248c8
chinahehaiyang <imhhy123@hotmail.com>
2018-11-20 20:21:58 -0700

    should have updateFromPosition function?

--- src/resourcemanager.h
+++ src/resourcemanager.h
@@ -28,7 +28,6 @@ class ResourceManager
 	iow::Camera m_camera_config;
 	iow::BulletConfig m_bullet_config;
 	std::vector<iow::DestroyableWallConfig> m_destroyable_wall_configs;
-	iow::BulletConfig m_bullet_config;
 
 	iow::TileMapConfig m_tile_map_config;
 	std::vector<TileConfig> m_tile_configs;
