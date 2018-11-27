e75f25c5e745066e09b2710e09e6ba23f672d3d9
jaredponn <jpon1234@gmail.com>
2018-11-27 16:13:16 -0700

    higher level abstraction for interfacing game input and raw game inputs

--- src/gameinput.h
+++ src/gameinput.h
@@ -2,7 +2,15 @@
 
 #include <vector>
 #include <tuple>
+#include <SFML/Graphics.hpp>
+
 #include "input.h"
+
+
+/** gameinput.h
+ * Enums and related data types for game inputs and game events. For parsing the
+ * events / causing the effects, look at the parsegamekeys.h/.cpp file
+ */
 
 namespace iow
 {
@@ -12,14 +20,22 @@ enum class PlayerGameEvents : unsigned int {
 	MOVE_PLAYER_RIGHT,
 	MOVE_PLAYER_LEFT,
 	MOVE_PLAYER_DOWN,
+
+	MOVE_PLAYER_UP_LEFT,
+	MOVE_PLAYER_DOWN_LEFT,
+	MOVE_PLAYER_UP_RIGHT,
+	MOVE_PLAYER_DOWN_RIGHT,
 	PLAYER_SHOOT,
 
 	NO_ACTION
 };
 
-using KeyBind = std::pair<iow::KeyState, iow::PlayerGameEvents>;
+using SingleKeyBind = std::pair<iow::KeyState, iow::PlayerGameEvents>;
 
-using KeyChord = std::tuple<std::vector<iow::KeyState>, PlayerGameEvents>;
+// key,
+using SFKeyAndKeyStatePair = std::pair<sf::Keyboard::Key, iow::KeyState>;
 
+using KeyChord =
+	std::pair<std::vector<SFKeyAndKeyStatePair>, iow::PlayerGameEvents>;
 
 } // namespace iow
