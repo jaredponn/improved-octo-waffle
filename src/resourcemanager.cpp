921b3db143bee425cf0ae00a26fb60f143646beb
jaredponn <jpon1234@gmail.com>
2018-12-20 21:16:27 -0700

    tiles were aded

--- src/resourcemanager.cpp
+++ src/resourcemanager.cpp
@@ -10,18 +10,18 @@ iow::ResourceManager::ResourceManager()
 {
 	/* keybind */
 	m_key_binds.resize(iow::MAX_NUMBER_KEYS,
-			   std::make_pair(iow::KeyState::INVALID_STATE,
-					  iow ::PlayerGameEvents::NO_ACTION));
-	m_key_binds[sf::Keyboard::W] = std::make_pair(
-		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_UP);
-	m_key_binds[sf::Keyboard::A] = std::make_pair(
-		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_LEFT);
-	m_key_binds[sf::Keyboard::S] = std::make_pair(
-		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_DOWN);
-	m_key_binds[sf::Keyboard::D] = std::make_pair(
-		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_RIGHT);
-	m_key_binds[sf::Keyboard::Space] = std::make_pair(
-		iow::KeyState::PRESSED, iow::PlayerGameEvents::PLAYER_SHOOT);
+			   {iow::KeyState::INVALID_STATE,
+			    iow ::PlayerGameEvents::NO_ACTION});
+	m_key_binds[sf::Keyboard::W] = {iow::KeyState::DOWN,
+					iow::PlayerGameEvents::MOVE_PLAYER_UP};
+	m_key_binds[sf::Keyboard::A] = {
+		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_LEFT};
+	m_key_binds[sf::Keyboard::S] = {
+		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_DOWN};
+	m_key_binds[sf::Keyboard::D] = {
+		iow::KeyState::DOWN, iow::PlayerGameEvents::MOVE_PLAYER_RIGHT};
+	m_key_binds[sf::Keyboard::Space] = {
+		iow::KeyState::PRESSED, iow::PlayerGameEvents::PLAYER_SHOOT};
 
 	/* key chords */
 	m_key_chords.push_back(std::make_pair(
@@ -55,8 +55,8 @@ iow::ResourceManager::ResourceManager()
 	m_textures.loadTextureFromFile("staticwall", "../resources/wall.png");
 	m_textures.loadTextureFromFile("bullet", "../resources/muted.png");
 
-
 	/* player stuff */
+	m_player_config.collisionBox = sf::RectangleShape(sf::Vector2f(32, 32));
 	m_player_config.bulletInterval = 10;
 	m_player_config.hp = 10;
 	m_player_config.spawnPosition = sf::Vector2f(250, 250);
@@ -91,22 +91,24 @@ iow::ResourceManager::ResourceManager()
 	m_camera_config.scale = sf::Vector2f(1.0f, -1.0f);
 
 	/* Tile map */
-	m_tile_map_config.setTileSize(
-		100,
-		100); // must be set before everything else
+	// must be set before everything else
+	m_tile_map_config.setTileSize(100, 100);
 
 	/* Indiviudal tile configs  */ // must be declared before
 				       // loading the tile maps
 	auto groundConf = std::make_optional<TileConfig>(
-		TileConfig(m_textures.getTexture("ground")));
+		m_textures.getTexture("ground"), std::nullopt, std::nullopt);
 	m_tile_map_config.setTileConfig(0, std::move(groundConf));
 
+	auto colLayer = std::make_optional<iow::TileCollisionLayer>(
+		sf::Vector2f(100, 100));
 	auto staticWallConf = std::make_optional<TileConfig>(
-		TileConfig(m_textures.getTexture("staticwall")));
+		m_textures.getTexture("staticwall"), colLayer, std::nullopt);
+
 	m_tile_map_config.setTileConfig(1, std::move(staticWallConf));
 
-	m_tile_map_config.loadTileMap(
-		iow::DEFAULT_TILE_MAP); // loading the tile maps
+	// loading the tile maps
+	m_tile_map_config.loadTileMap(iow::DEFAULT_TILE_MAP);
 
 	/* bullet configs */
 	m_bullet_config.speed = 2.f;
@@ -114,6 +116,6 @@ iow::ResourceManager::ResourceManager()
 	m_bullet_config.sprite.setTexture(m_textures.getTexture("bullet"));
 	m_bullet_config.sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
 	m_bullet_config.sprite.setColor(sf::Color::Yellow);
-} // namespace iow
+}
 
 } // namespace iow
