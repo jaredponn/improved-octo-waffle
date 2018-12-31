e35145e2686a5a2c2958cb01e78cc27276fabb4e
jaredponn <jpon1234@gmail.com>
2018-12-30 22:11:38 -0700

    updated file to include bullet deletion game events. This is  the file that should be edited when creating new internal game events

--- src/internalgameevents.h
+++ src/internalgameevents.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <stack>
+
 namespace iow
 {
-enum class InternalGameEvents { DELETE_BULLET };
-}
+
+struct DeleteBullet {
+	size_t bulletEntity;
+};
+
+// Sum type of interanl game events
+enum class InternalGameEventType { DELETE_BULLET };
+struct InternalGameEvent {
+	iow::InternalGameEventType type;
+	union {
+		DeleteBullet deleteBullet;
+	};
+};
+} // namespace iow
