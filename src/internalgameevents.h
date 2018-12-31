9947a36d34d990516051118ed8a7b7cbf1eb9889
chinahehaiyang <imhhy123@hotmail.com>
2018-12-31 10:43:34 -0700

    added knockback into internalGameEvent

--- src/internalgameevents.h
+++ src/internalgameevents.h
@@ -8,13 +8,17 @@ namespace iow
 struct DeleteBullet {
 	size_t bulletEntity;
 };
+struct PlayerKnockback {
+	size_t playerIndex;
+};
 
 // Sum type of interanl game events
-enum class InternalGameEventType { DELETE_BULLET };
+enum class InternalGameEventType { DELETE_BULLET, PLAYER_KNOCKBACK };
 struct InternalGameEvent {
 	iow::InternalGameEventType type;
 	union {
 		DeleteBullet deleteBullet;
+		PlayerKnockback playerKnockback;
 	};
 };
 } // namespace iow
