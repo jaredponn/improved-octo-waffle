d691f78d809f5d089bffdbdb75b4d3038d8a5c84
jaredponn <jpon1234@gmail.com>
2019-01-04 10:44:42 -0700

    introduction of tilemap test

--- tests/tilemaptests.cpp
+++ tests/tilemaptests.cpp
@@ -1,24 +1,10 @@
 #include "../extern/catch.hpp"
-#include "../src/tilemap.h"
+#include "../src/graph.h"
 
-TEST_CASE("TileMap internal getters and setters of coordinates")
+TEST_CASE("Graphs referencing and dereferencing tests")
 {
 
-	// to
-	iow::TileCoordi dimensions = iow::TileCoordi{5, 10};
+	iow::Graph<int, 2> testGraph{};
 
-	auto a = iow::TileMap::getTileCoord(1, dimensions);
-	REQUIRE(iow::TileMap::getTileIndex(a, dimensions) == 1);
-
-	a = iow::TileMap::getTileCoord(4, dimensions);
-	REQUIRE(iow::TileMap::getTileIndex(a, dimensions) == 4);
-
-	// from
-	auto b = iow::TileMap::getTileIndex(iow::TileCoordi{2, 3}, dimensions);
-	REQUIRE(iow::TileMap::getTileCoord(b, dimensions)
-		== iow::TileCoordi{2, 3});
-
-	b = iow::TileMap::getTileIndex(iow::TileCoordi{8, 2}, dimensions);
-	REQUIRE(iow::TileMap::getTileCoord(b, dimensions)
-		== iow::TileCoordi{8, 2});
+	REQUIRE(1 == 1);
 }
