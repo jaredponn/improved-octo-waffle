9ac1c7b9fa18587034dd5be8e58fa8fb09b0a37a
jaredponn <jpon1234@gmail.com>
2019-01-04 22:54:48 -0700

    tile map tests for dijkstras

--- tests/graphtests.cpp
+++ tests/graphtests.cpp
@@ -1,24 +1,121 @@
 #include "../extern/catch.hpp"
-#include "../src/tilemap.h"
+#include "../src/graph.h"
+#include "../src/pathfinding.h"
 
-TEST_CASE("TileMap internal getters and setters of coordinates")
+
+TEST_CASE("Graphs referencing and dereferencing tests")
 {
+	constexpr size_t MAX_NEIGHBORS = 2;
+	iow::Graph<int, MAX_NEIGHBORS> testGraph{};
 
-	// to
-	iow::TileCoordi dimensions = iow::TileCoordi{5, 10};
+	testGraph.m_graph.push_back(std::make_pair(
+		0, iow::GraphNeighbors<MAX_NEIGHBORS>(
+			   {iow::makeGraphEdge(1), iow::makeGraphEdge(2)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		1, iow::GraphNeighbors<MAX_NEIGHBORS>(
+			   {iow::makeGraphEdge(2), iow::makeGraphEdge(3)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		2, iow::GraphNeighbors<MAX_NEIGHBORS>(
+			   {iow::makeGraphEdge(3),
+			    iow::makeGraphEdge(iow::INVALID_GRAPH_COORD)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		3, iow::GraphNeighbors<MAX_NEIGHBORS>(
+			   {iow::makeGraphEdge(iow::INVALID_GRAPH_COORD),
+			    iow::makeGraphEdge(iow::INVALID_GRAPH_COORD)})));
 
-	auto a = iow::TileMap::getTileCoord(1, dimensions);
-	REQUIRE(iow::TileMap::getTileIndex(a, dimensions) == 1);
+	REQUIRE(testGraph.dereferenceDataGraphCoord(0) == 0);
+	REQUIRE(testGraph.dereferenceDataGraphCoord(1) == 1);
+	REQUIRE(testGraph.dereferenceDataGraphCoord(2) == 2);
+	REQUIRE(testGraph.dereferenceDataGraphCoord(3) == 3);
 
-	a = iow::TileMap::getTileCoord(4, dimensions);
-	REQUIRE(iow::TileMap::getTileIndex(a, dimensions) == 4);
+	REQUIRE(testGraph.referenceGraphCoord(
+			testGraph.dereferenceVertexGraphCoord(0))
+		== 0);
+	REQUIRE(testGraph.referenceGraphCoord(
+			testGraph.dereferenceVertexGraphCoord(1))
+		== 1);
+	REQUIRE(testGraph.referenceGraphCoord(
+			testGraph.dereferenceVertexGraphCoord(2))
+		== 2);
+	REQUIRE(testGraph.referenceGraphCoord(
+			testGraph.dereferenceVertexGraphCoord(3))
+		== 3);
+}
 
-	// from
-	auto b = iow::TileMap::getTileIndex(iow::TileCoordi{2, 3}, dimensions);
-	REQUIRE(iow::TileMap::getTileCoord(b, dimensions)
-		== iow::TileCoordi{2, 3});
+TEST_CASE("Dijstraks test case 0")
+{
+	constexpr size_t MAX_NEIGHBORS = 1;
+	iow::Graph<char, MAX_NEIGHBORS> testGraph{};
+	testGraph.m_graph.push_back(
+		std::make_pair('A', iow::GraphNeighbors<MAX_NEIGHBORS>(
+					    {iow::makeGraphEdge(1, 3)})));
+	testGraph.m_graph.push_back(
+		std::make_pair('B', iow::GraphNeighbors<MAX_NEIGHBORS>(
+					    {iow::makeGraphEdge(0, 3)})));
 
-	b = iow::TileMap::getTileIndex(iow::TileCoordi{8, 2}, dimensions);
-	REQUIRE(iow::TileMap::getTileCoord(b, dimensions)
-		== iow::TileCoordi{8, 2});
+
+	REQUIRE(iow::Pathfinding::dijkstras(testGraph, 0, 1) == 3);
+}
+
+TEST_CASE("Dijstraks test case 1")
+{
+	constexpr size_t MAX_NEIGHBORS = 2;
+	iow::Graph<char, MAX_NEIGHBORS> testGraph{};
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'A',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 5), iow::makeGraphEdge(2, 6)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'B',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 5), iow::makeGraphEdge(3, 3)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'C',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 6), iow::makeGraphEdge(3, 1)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'D',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 3), iow::makeGraphEdge(2, 1)})));
+
+	REQUIRE(iow::Pathfinding::dijkstras(testGraph, 0, 3) == 7);
+}
+
+
+TEST_CASE("Dijstraks test case 2")
+{
+	constexpr size_t MAX_NEIGHBORS = 4;
+	iow::Graph<char, MAX_NEIGHBORS> testGraph{};
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'A',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 3), iow::makeGraphEdge(2, 1),
+			 iow::INVALID_GRAPH_EDGE, iow::INVALID_GRAPH_EDGE})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'B',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 3), iow::makeGraphEdge(2, 7),
+			 iow::makeGraphEdge(3, 5), iow::makeGraphEdge(4, 1)})));
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'C',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 1), iow::makeGraphEdge(1, 7),
+			 iow::makeGraphEdge(3, 2), iow::INVALID_GRAPH_EDGE})));
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'D',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(2, 2), iow::makeGraphEdge(1, 5),
+			 iow::makeGraphEdge(4, 7), iow::INVALID_GRAPH_EDGE})));
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'E',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 1), iow::makeGraphEdge(3, 7),
+			 iow::INVALID_GRAPH_EDGE, iow::INVALID_GRAPH_EDGE})));
+
+	REQUIRE(iow::Pathfinding::dijkstras(testGraph, 2, 4) == 5);
 }
