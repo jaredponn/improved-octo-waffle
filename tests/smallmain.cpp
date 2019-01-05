471a441b95f632e514a0d2b408ada8b16aa12d44
jaredponn <jpon1234@gmail.com>
2019-01-04 22:55:07 -0700

    small main file for isolated testing

---
+++ tests/smallmain.cpp
@@ -0,0 +1,30 @@
+#include "../src/graph.h"
+#include "../src/pathfinding.h"
+#include <iostream>
+
+int main(void)
+{
+
+	constexpr size_t MAX_NEIGHBORS = 2;
+	iow::Graph<char, MAX_NEIGHBORS> testGraph{};
+
+	testGraph.m_graph.push_back(std::make_pair(
+		'A',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 5), iow::makeGraphEdge(2, 6)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'B',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 5), iow::makeGraphEdge(3, 3)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'C',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(0, 6), iow::makeGraphEdge(3, 1)})));
+	testGraph.m_graph.push_back(std::make_pair(
+		'D',
+		iow::GraphNeighbors<MAX_NEIGHBORS>(
+			{iow::makeGraphEdge(1, 3), iow::makeGraphEdge(2, 1)})));
+
+	std::cout << iow::Pathfinding::dijkstras(testGraph, 0, 3);
+	return 0;
+}
